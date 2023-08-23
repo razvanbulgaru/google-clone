d96c48bdddb73499fdfc9d4f1db3a0041fc54f82
Razvan Bulgaru <razvan.bulgaru@gmail.com>
2023-08-23 10:54:53 +0300

    Add home header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
+import HomeHeader from '@/components/HomeHeader';
 import Image from 'next/image';
 
 export default function Home() {
-	return <div className="text-red-600 text-3xl">HOME</div>;
+	return (
+		<>
+			<HomeHeader />
+		</>
+	);
 }
