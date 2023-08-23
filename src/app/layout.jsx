c5c4fefe9dbf0a5c4ace245894802c9eaf6e06a1
Razvan Bulgaru <razvan.bulgaru@gmail.com>
2023-08-23 10:22:29 +0300

    Add tailwind css support

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,13 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Favicon from '/public/favicon.svg';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
 	title: 'Google Clone',
 	description: 'Google clone created with nextjs13`',
+	icons: [{ rel: 'icon', url: Favicon.src }],
 };
 
 export default function RootLayout({ children }) {
