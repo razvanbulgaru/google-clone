b9ad715616c938fd40fd58df3e982efb2183dfb3
Razvan Bulgaru <razvan.bulgaru@gmail.com>
2023-08-23 19:55:37 +0300

    Fetch data from google search API

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const WebSearchPage = () => {
-	return <div>WebSearchPage</div>;
+const WebSearchPage = async ({ searchParams }) => {
+	const res = await fetch(
+		`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+	);
+	const data = await res.json();
+	const results = data.items;
+	return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 };
 
 export default WebSearchPage;
